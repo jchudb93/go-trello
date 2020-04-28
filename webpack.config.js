be636321b74496b302c49a7bf3230985610ba10d
Jose Chu <jchudb93@gmail.com>
2020-04-27 22:37:25 -0500

    webpack basic

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+
+function join(dest) { return path.resolve(__dirname, dest); }
+
+function web(dest) { return join('web/static/' + dest); }
+
+var config = module.exports = {
+  entry: {
+    application: [
+      web('css/application.sass'),
+      web('js/application.js'),
+    ],
+  },
+
+  output: {
+    path: join('priv/static'),
+    filename: 'js/application.js',
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.sass'],
+    modulesDirectories: ['node_modules'],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('css/application.css'),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  );
+}
